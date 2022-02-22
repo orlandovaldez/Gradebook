21760e1f27fbb6883c84497bd3bb5d23174a692b
Orlando Valdez <60371970+orlandovaldez@users.noreply.github.com>
2022-02-22 05:25:28 +0000

    appended list items

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["arc
 subjects = ["physics", "calculus", "poetry", "history"]
 grades = [98, 97, 85, 88]
 gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+print(gradebook)
+gradebook.append(["computer science", 100])
+gradebook.append(["visual arts", 93])
