7eddc0716545601737507ed9238aef7066f174d6
Orlando Valdez <60371970+orlandovaldez@users.noreply.github.com>
2022-02-22 05:23:32 +0000

    added two dimensional gradebook list

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["arc
 # Your code below: 
 subjects = ["physics", "calculus", "poetry", "history"]
 grades = [98, 97, 85, 88]
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
