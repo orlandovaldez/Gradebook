519b3aa54e6897603d54173f0ef34460519477ea
Orlando Valdez <60371970+orlandovaldez@users.noreply.github.com>
2022-02-23 03:36:09 +0000

    Print full_gradebook

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
 print(gradebook)
 gradebook.append(["computer science", 100])
 gradebook.append(["visual arts", 93])
+gradebook[-1][-1] +=5
+print(gradebook)
+gradebook[2].remove(85)
+gradebook[2].append("Pass")
+
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
